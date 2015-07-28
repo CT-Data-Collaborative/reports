dcf6538df8d3f441ac438fa3c92841265180b26c
Sasha Cuerda <sasha@mapconomy.com>
2015-07-28 10:36:37 -0400

    added inline template

--- scripts/naked_test.py
+++ scripts/naked_test.py
@@ -1,5 +1,6 @@
 from Naked.toolshed.shell import run_js
 from base64 import b64encode
+from jinja2 import Template
 
 # helper for encoding svg so that we can hand off to weasy
 
@@ -14,3 +15,6 @@ print(success)
 
 # print encoded svg
 print (svg2data_url(success))
+
+template = Template('<!DOCTYPE html><html lang="en"><head><title>Report Test</title></head><body><h1> CERC Town Profile</h1>{{ svg }}</body></html>')
+template.render(svg=success)
