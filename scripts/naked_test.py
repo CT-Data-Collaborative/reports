598beb994fdcf235c564dc8f7e81b06c5ab855fb
Sasha Cuerda <sasha@mapconomy.com>
2015-07-28 17:18:08 +0200

    rendering to pdf

--- scripts/naked_test.py
+++ scripts/naked_test.py
@@ -1,7 +1,7 @@
 from Naked.toolshed.shell import run_js
 from base64 import b64encode
 from jinja2 import Template
-
+from weasyprint import HTML
 # helper for encoding svg so that we can hand off to weasy
 
 def svg2data_url(value):
@@ -16,5 +16,7 @@ print(success)
 # print encoded svg
 print (svg2data_url(success))
 
-template = Template('<!DOCTYPE html><html lang="en"><head><title>Report Test</title></head><body><h1> CERC Town Profile</h1>{{ svg }}</body></html>')
-template.render(svg=success)
+template = Template('<!DOCTYPE html><html lang="en"><head><title>Report Test</title></head><body><h1> CERC Town Profile</h1><img src="{{ encoded_svg }}"></img></body></html>')
+rendered_template = template.render(encoded_svg=svg2data_url(success))
+HTML(string=rendered_template).write_pdf('donut.pdf')
+
