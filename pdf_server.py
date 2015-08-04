45655c5b855d5ad59ff80bb99f699914214b90d1
Brendan Swiniarski <bswiniarski@ctdata.org>
2015-08-04 13:49:45 -0400

    We now have a working pdf template that gets produced using a series of intermediary, node, and jinja calls. using Flask_WeasyPrint library to provide extra functionality, reduces code complexity as well.

--- pdf_server.py
+++ pdf_server.py
@@ -5,8 +5,9 @@ from flask import Flask, request, url_for, render_template, make_response #, ses
 ## Imports that i'm testing code for
 from Naked.toolshed.shell import muterun_js
 from base64 import b64encode
-from weasyprint import HTML
-import json
+# from weasyprint import HTML, CSS
+from flask_weasyprint import HTML, render_pdf
+import json, os
 from pipes import quote
 ## 
 
@@ -53,8 +54,6 @@ def post_test():
     response.headers["Content-Disposition"] = "attachment; filename=foo.pdf"
     return response
 
-# @app.route("/town_profile", methods=["GET", "POST"])
-# def town_profile():
 @app.route("/form", methods=["GET", "POST"])
 def form():
     print(request.method)
@@ -82,11 +81,48 @@ def form():
         obj["data"] = requestObj["data"]
         objects.append(obj);
 
-    return render_template("town_profile.html", objects = objects)
+    return render_template("form_output.html", objects = objects)
     # pdfResponse = HTML(string=render_template("town_profile.html", objects = objects)).write_pdf()
     # pdfResponse = make_response(pdfResponse)
     # pdfResponse.headers["Content-Disposition"] = "attachment; filename=town_profile.pdf"
     # return pdfResponse
+
+@app.route("/town_profile", methods=["GET", "POST"])
+def town_profile():
+    # build vis objects
+    req = json.loads('{"template":"town_profile","objects":[{"type":"table","name":"race","data":[["","Hartford","Connecticut"],["American Indian and Alaska Native Alone",596,8770],["Black or African American Alone",47786,361668],["Hispanic or Latino",54289,496939],["White Alone",43660,2792554],["White Alone, Not Hispanic or Latino",20892,2526401]],"config":{}},{"type":"table","name":"population","data":[["","Hartford","Connecticut"],[2015,125999,3644545],[2020,126656,3702469],[2025,126185,3746181]],"config":{}},{"type":"table","name":"age","data":[["","0 to 4 years","10 to 14 years","15 to 19 years","20 to 24 years","25 to 29 years","30 to 34 years","35 to 44 years","45 to 54 years","55 to 64 years","5 to 9 years","65 to 74 years","75 to 84 years","85 years and over","Total"],["Hartford",8487,8613,12832,12571,10721,9165,14801,14919,11710,9184,6968,3514,1645,125130],["Connecticut",197395,236742,255816,229708,217169,211089,469746,568510,456963,220139,269422,164260,86602,3583561]],"config":{}}]}')
+
+    objects = {}
+    for requestObj in req["objects"]:
+        obj = {}
+
+        # in the future we should just have these available for every type of chart we expect from every application use case
+        # so we don't have to check, we just ALWAYS call:
+        #        ` requestObj["data"] = transform_data(requestObj["data"], requestObj["type"])  `
+        if (requestObj["type"] == "table"):
+            requestObj["data"] = intermediary.transform_data(requestObj["data"], "table")
+
+        nodeResponse = muterun_js('scripts/visualizations/'+requestObj["type"]+'.js', "--data="+quote(json.dumps(requestObj["data"])))
+
+        obj["output"] = render_template(requestObj["type"]+".html", data = nodeResponse.stdout)
+
+        obj["className"] = requestObj["type"];
+        obj["dump"] = nodeResponse.stdout
+        obj["data"] = requestObj["data"]
+        objects[requestObj["name"]] = obj
+
+    # render template
+    response = render_template("town_profile.html", objects = objects)
+
+    # Temporarily using a get parameter ("print" = true)
+    # should be using request.method == POST, as commented out below.
+    # Or just returning print version by default, if we don't keep a screen view here.
+    # if request.method == "POST":
+    if request.args.get("print", default = False):
+        response = HTML(string=response)
+        response = render_pdf(response)
+
+    return response
 
 
 # run the application to public server
