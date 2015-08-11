63e2ed8f9829d31c37fcb4c787373172c25af496
Brendan Swiniarski <bswiniarski@ctdata.org>
2015-08-11 16:26:43 -0400

    Committed wrong script in previous commit. including correct script now. This is the rewrite of the table d3 script.

--- scripts/visualizations/table.js
+++ scripts/visualizations/table.js
@@ -1,43 +1,106 @@
-args = require("minimist")(process.argv.slice(2));
+/**
+ * Necessary Requirements
+ * d3 - charting library
+ * args - command line arguments, passed to node, processed by minimist
+ * jsdom - JSDOM library, for accessing browser-less virtual DOM
+ */
+var d3 = require("d3"),
+        minimist = require("minimist"),
+        jsdom = require("jsdom");
 
-var data = JSON.parse(args.data);
+/**
+ * Process arguments from node call using minimist
+ */
+var args = minimist(process.argv.slice(2)),
+        data = JSON.parse(args.data),
+        config = JSON.parse(args.config);
 
-var d3 = require('d3');
-var document = require('jsdom').jsdom();
-var body = d3.select(document.body);
+// get chart function object
+chart = tableChart();
 
-var width = 420,
-    cellWidth = width/data.columns.length,
-    cellHeight = "1.8em";
+// use available config parameters to override defaults
+// width
+// if ("width" in config && config.width > 0) {
+//     chart.width(config.width);
+// }
 
-var body = d3.select(document.body);
-
-var table = body.html('')
-        .append("table");
-
-var thead = table.append("thead")
-        .append("tr")
-        .selectAll("th")
-        .data(data.columns).enter()
-        .append("th")
-        .text(function(c) { return c; } );
-
-var tbody = table.append("tbody");
-
-var rows = tbody.selectAll("tr")
-        .data(data.rows)
-        .enter()
-        .append("tr");
-
-// create a cell in each row for each column
-var cells = rows.selectAll("td")
-    .data(function(row) {
-        return data.columns.map(function(column, index) {
-            return {column: column, value: row[index]};
-        });
-    })
-    .enter()
-    .append("td")
-    .text(function(d) { return d.value; });
+// get body from jsdom, call chart function
+var document = jsdom.jsdom();
+var body = d3.select(document.body)
+            .html("")
+            .datum(data)
+            .call(chart);
 
 console.log(body.html());
+
+function tableChart() {
+    // Vars
+    // var width = 460;
+    
+    function chart(selection) {
+        selection.each(function(data) {
+
+            // Convert data to standard representation greedily;
+            // this is needed for nondeterministic accessors.
+            data = data;
+            // data = data.map(function(d, i) {
+            //     return [label.call(data, d, i), value.call(data, d, i)];
+            // });
+
+            // outermost table Container
+            var table = d3.select(this).append("table")
+
+            // table header
+            var thead = table.append("thead")
+                    .append("tr")
+                    .selectAll("th")
+                    .data(data.columns).enter()
+                    .append("th")
+                        .text(function(c) { return c; } );
+
+            // tbody element
+            var tbody = table.append("tbody");
+
+            // tr elements
+            var rows = tbody.selectAll("tr")
+                    .data(data.rows)
+                    .enter()
+                    .append("tr");
+
+            // create a cell in each row for each column
+            var cells = rows.selectAll("td")
+                .data(function(row) {
+                    return data.columns.map(function(column, index) {
+                        return {column: column, value: row[index]};
+                    });
+                })
+                .enter()
+                .append("td")
+                .text(function(d) { return d.value; });
+        });
+    }
+
+    /**
+     * Getter-Setter functions for chart function object
+     */
+    // chart.width = function(_) {
+    //     if (!arguments.length) return width;
+    //     width = _;
+    //     return chart;
+    // };
+
+    // These will probably never be used, but keeping for posterity
+    chart.label = function(_) {
+        if (!arguments.length) return label;
+        label = _;
+        return chart;
+    };
+
+    chart.value = function(_) {
+        if (!arguments.length) return value;
+        value = _;
+        return chart;
+    };
+
+    return chart;
+}
