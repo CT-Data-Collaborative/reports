a1ca0f9b182bea1d4a67d8bff827c794613b3861
Brendan Swiniarski <brendan.james.swiniarski@gmail.com>
2015-09-09 10:26:25 -0400

    Grouped bar chart is implemented - not sure if we'll keep this in the same file, as this currently breaks regular bar charts. Using new CKAN-esque data format and will have to reconfigure all d3 scripts accordingly.

--- scripts/visualizations/bar.js
+++ scripts/visualizations/bar.js
@@ -33,14 +33,19 @@ for (var type in config.formats) {
 chart = barChart();
 
 // use available config parameters to override defaults
-// barHeight
-if ("barHeight" in config && config.barHeight > 0) {
-    chart.barHeight(config.barHeight);
+// height
+if ("height" in config && config.height > 0) {
+    chart.height(config.height);
 }
 
 // width
 if ("width" in config && config.width > 0) {
     chart.width(config.width);
+}
+
+// barWidth
+if ("barWidth" in config && config.barWidth > 0) {
+    chart.barWidth(config.barWidth);
 }
 
 //Color scale
@@ -58,49 +63,127 @@ var body = d3.select(document.body)
 console.log(body.html());
 
 function barChart() {
-    var width = 460,
-            barHeight = 20,
-            x = d3.scale.linear()
-                            // .domain([0, d3.max(data, function(d) { return d[1]; })])
-                            .domain([0, width])
-                            .range([0, width]);
-            colors = d3.scale.category20();
+            // VARS
+            // dims
+    var margin = {top: 20, right: 20, bottom: 20, left: 20},
+            width = 480 - margin.left - margin.right,
+            height = 250 - margin.top - margin.bottom,
+
+            // scales
+            x0 = d3.scale.ordinal()
+                        .rangeRoundBands([0, width], 0.1),
+            x1 = d3.scale.ordinal(),
+            y = d3.scale.linear()
+                    .range([height, 0]),
+            color = d3.scale.category20(),
+
+            // axes
+            xAxis = d3.svg.axis()
+                .scale(x0)
+                .orient("bottom"),
+
+            // bar widths
+            defaultBarWidth = true,
+            barWidth = 20;
 
     function chart(selection) {
         selection.each(function(data) {
 
-            // Convert data to standard representation greedily;
-            // this is needed for nondeterministic accessors.
-            data = data;
-            // data = data.map(function(d, i) {
-            //     return [label.call(data, d, i), value.call(data, d, i)];
-            // });
+            var fields = {};
+            data.fields.forEach(function(field) {
+                fields[field.id] = field;
+            });
 
-            // set domain of scale appropriate to data
-            x.domain([0, d3.max(data, function(d) { return d[1].value; })])
+            // Should this be a parameter? passed in config?
+            var grouping = "Quarter";
+
+            var groupLabels = d3.keys(data.records[0]).filter(function(key) { return key !== grouping && key !== "_id"; });
+
+            // This is the step that seems to be the most confused and broken - what shape am i aiming for?
+            data.records.forEach(function(d) {
+                d.values = groupLabels.map(function(label) {
+                    return {name: label, value: formatters[fields[label]["type"]](d[label])};
+                });
+            });
+
+
+            //  set domain for group scale
+            x0.domain(data.records.map(function(d) { return d[grouping]; }));
+
+            // set domain and range banding for scale for bars within groups
+            x1.domain(groupLabels)
+                .rangeRoundBands([0, x0.rangeBand()]);
+
+            // set y-scale domain 
+            y.domain([0, d3.max(data.records, function(d) { return d3.max(d.values, function(d) { return d.value; }); })]);
+
+            // container, margined interior container
+            var svg = d3.select(this).append('svg')
+                    .attr("xmlns", "http://www.w3.org/2000/svg")
+                    .attr("height", height + margin.top + margin.bottom )
+                    .attr("width", width + margin.left + margin.right )
+                .append("g")
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+            // x axis, includes group labels automatically
+            svg.append("g")
+                  .attr("transform", "translate(0," + height + ")")
+                  .attr("font-size", "8pt")
+                  .call(xAxis);
+
+            // group containers
+            var groups = svg.selectAll(".group")
+                    .data(data.records)
+                    .enter()
+                        .append("g")
+                        .attr("transform", function(d) { return "translate(" + x0(d[grouping]) + ", 0)"});
+
+            // bars within groups
+            groups.selectAll("rect")
+                    .data(function(d) { return d.values; })
+                    .enter()
+                        .append("rect")
+                            .attr("width", x1.rangeBand())
+                            .attr("x", function(d) { return x1(d.name); })
+                            .attr("y", function(d) { return y(d.value); })
+                            .attr("height", function(d) { return height - y(d.value); })
+                            .style("fill", function(d) { return color(d.name); });
+
+
+            /* Old Code */
+            /*
+            if (defaultBarWidth) {
+                barWidth = width / data.length;
+            }
+
 
             var svg = d3.select(this).append('svg')
-                .attr("width", width)
-                .attr("height", barHeight * data.length)
-                .attr("xmlns", "http://www.w3.org/2000/svg");
+                    .attr("xmlns", "http://www.w3.org/2000/svg")
+                    .attr("height", height + margin.top + margin.bottom )
+                    .attr("width", width + margin.left + margin.right )
+                .append("g")
+                    .attr("width", width)
+                    .attr("height", height)
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");;
 
             var bar = svg.selectAll("g")
                 .data(data)
               .enter().append("g")
-                .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+                .attr("transform", function(d, i) { return "translate("+ i * barWidth + ", 0)"; });
 
             bar.append("rect")
-                .attr("width", function(d) { return x(d[1].value); })
-                .attr("height", barHeight - 1)
+                .attr("height", function(d) { return y(d[1].value); })
+                .attr("width", barWidth - 1)
                 .attr("fill", "steelblue");
 
             bar.append("text")
-                // .attr("x", function(d) { return x(d) - 3; }) // offset for text labels
-                .attr("x", 0)
-                .attr("y", barHeight / 2)
+                // .attr("y", function(d) { return y(d) - 3; }) // offset for text labels
+                .attr("x", barWidth / 2)
+                .attr("y", 0)
                 .attr("dy", ".35em")
                 .attr("fill", "white")
                 .text(function(d) { return d[0].value; });
+        */
         });
     }
 
@@ -109,19 +192,19 @@ function barChart() {
      */
     chart.width = function(_) {
         if (!arguments.length) return width;
-        width = _;
+        width = _ - margin.left - margin.right;
         return chart;
     };
 
     chart.height = function(_) {
         if (!arguments.length) return height;
-        height = _;
+        height = _ - margin.top - margin.bottom;
         return chart;
     };
 
-    chart.barHeight = function(_) {
-        if (!arguments.length) return barHeight;
-        barHeight = _;
+    chart.barWidth = function(_) {
+        if (!arguments.length) return barWidth;
+        barWidth = _;
         return chart;
     };
 
