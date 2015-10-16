2e066e53d73957ae2f814c3b6a7ac67d91228016
Brendan Swiniarski <brendan.james.swiniarski@gmail.com>
2015-10-16 16:15:33 -0400

    New template and accompanying intermediary script. Intermediary script does NO processing or manipulation. Template simply loops through all available data objects and outputs to body. Very useful for testing scripts and request objects outside of the context of an actual report like CONNECT or town profile. Table script now consumes complex data, allows and accounts for nesting on one or more levels. Has been tested with up to two nest keys, but in theory can handle any number. Connect mock request is updated with complex table data.

---
+++ scripts/intermediate/test.py
@@ -0,0 +1,25 @@
+import json
+
+def table(visObj):
+    return visObj
+
+def pie(visObj):
+    return visObj
+
+def map(visObj):
+    return visObj
+
+def bar(visObj):
+    return visObj
+
+transformations = {"table" : table, "pie" : pie, "map" : map, "bar" : bar}
+
+def get_extra_obj(data):
+    extra_obj = []
+
+    return extra_obj
+
+def get_info(data):
+    info = {}
+
+    return info
--- scripts/visualizations/table.js
+++ scripts/visualizations/table.js
@@ -6,8 +6,7 @@
  */
 var d3 = require("d3"),
         minimist = require("minimist"),
-        jsdom = require("jsdom"),
-        jetpack = require("../../node_modules/d3-jetpack/d3-jetpack.js")(d3);
+        jsdom = require("jsdom");
 
 /**
  * Process arguments from node call using minimist
@@ -61,9 +60,55 @@ function tableChart() {
             // this is needed for nondeterministic accessors.
             data = data;
 
-            // data = data.map(function(d, i) {
-            //     return [label.call(data, d, i), value.call(data, d, i)];
-            // });
+            /*
+            var priority_order = ['MUST', "SHOULD", 'COULD', 'WISH'];
+            var nested_data = d3.nest()
+            .key(function(d) { return d.status; }).sortKeys(d3.ascending)
+            .key(function(d) { return d.priority; }).sortKeys(function(a,b) { return priority_order.indexOf(a) - priority_order.indexOf(b); })
+            .rollup(function(leaves) { return leaves.length; })
+            .entries(csv_data);
+            */
+            if ("nest" in config) {
+                nestedData = d3.nest();
+
+                config.nest.forEach(function(key, keyInd, keyArr) {
+                    if ("order" in config && key in config.order) {
+                        nestedData.key(function (d) { return formatters[d[key].type](d[key].value); })
+                                .sortKeys(function(a, b) {
+                                    return config.order[key].indexOf(a) - config.order[key].indexOf(b);
+                                });
+                    } else {
+                        nestedData.key(function (d) { return d[key]; });
+                    }
+                });
+
+                nestedData.rollup(function(leaf) {
+                    leaf = leaf.pop();
+                    for (key in leaf) {
+                        if (config.nest.indexOf(key) !== -1) {
+                            delete leaf[key];
+                        }
+                    }
+                    return leaf;
+                });
+
+                data = nestedData.map(data.records)
+            }
+
+            var countCols = function(nest) {
+                if (typeof nest === "object") {
+                    return d3.keys(nest).length * countCols(nest[d3.keys(nest)[0]]);
+                } else {
+                    return 1;
+                }
+            }
+            var nrows = d3.keys(data).length,
+                    ncols = countCols(data) / (2 * nrows);
+
+            // useful debugging of nest functions
+            // var container = d3.select(this).append("pre")
+            //     .text(JSON.stringify(data, null, 4));
+            // return chart;
 
             // outermost container
             var container = d3.select(this).append("div")
@@ -86,65 +131,54 @@ function tableChart() {
             // bump down by wordwrap
             table.attr("style", "top:"+(marginBump * 6)+"px;")
 
-            // Calculate colspan
-            // if header cells < data cells, per row.
-            var noblankColumns = data.fields.filter(function(col) { return col.id !== "" & col.id !== "_id"})
-
-            if (noblankColumns.length < data.records[0].length-1 && noblankColumns.length > 0) {
-                colspan = Math.floor((data.records[0].length)/(noblankColumns.length))
-                colspan = (colspan > 1 ? colspan : null)
-            }
-
-            
             // table header
-            var thead = table.append("thead")
-                    .append("tr")
-                    .selectAll("th")
-                    .data(noblankColumns).enter()
-                    .append("th")
-                        .text(function(d) { return d.id; } )
-                    .attr("colspan", function(d, i) {
-                        if (i > 0 || d.value != "") {
-                            return colspan;
-                        } else {
-                            return null;
-                        }
-                    });
+            var thead = table.append("thead");
 
-            fields = {}
-            data.fields.forEach(function(field) {
-                fields[field.id] = field;
-            });
+            // now populate thead with th cells appropriately
 
             // tbody element
             var tbody = table.append("tbody");
 
-            // tr elements
-            var rows = tbody.selectAll("tr")
-                    .data(data.records)
-                    .enter()
-                    .append("tr");
+            populateHeader = function(data, thead, level) {
+                if (data instanceof Object && data[d3.keys(data)[0]] instanceof Object) {
+                    var theadTR = thead.selectAll("tr#level_"+level);
+                    if (theadTR.empty()) {
+                        theadTR = thead.append("tr")
+                                                    .attr("id", "level_"+level);
+                        theadTR.append("th");
+                    }
+                    for (var key in data) {
+                        theadTR.append("th")
+                            .text(key)
+                            .attr("colspan", d3.keys(data[key]).length);
+                            populateHeader(data[key], thead, level + 1)
+                    }
+                } else {
+                    thead.selectAll("tr#level_"+(level-1)).selectAll("th")
+                            .attr("colspan", 1);
+                    return
+                }
+            }
 
-            // create a cell in each row for each column
-            var cells = rows.selectAll("td")
-                .data(function(d) {
-                    row = [];
-                    noblankColumns.forEach(function(field) {
-                        row.push({key : field.id, type: d[field.id].type, value : formatters[d[field.id].type](d[field.id].value)});
-                    });
-                    return row;
-                })
-                .enter()
-                .append("td")
-                .text(function(d) { return d.value; })
-                .attr("class", function(d) { return d.type; })
-                .attr("colspan", function(d, i) { return colspan && (i > 0 || data.fields[i].value != "") ? 1 : null });
+            populateHeader(data[d3.keys(data)[0]], thead, 0);
 
-            if ("source" in config && config.source !== "") {
-                // source
-                var source = container.append("p")
-                    .attr("class", "table_source")
-                    .text(config.source);
+            // populate body
+            populateCells = function(data, thead, tr, level) {
+                if (data instanceof Object && data[d3.keys(data)[0]] instanceof Object) {
+                    for (var key in data) {
+                        populateCells(data[key], thead, tr, level + 1);
+                    }
+                } else {
+                    tr.append("td")
+                        .text(formatters[data.type](data.value));
+                }
+            }
+
+            for (rowKey in data) {
+                var tr = tbody.append("tr");
+                tr.append("td")
+                    .text(rowKey);
+                populateCells(data[rowKey], thead, tr, 0);
             }
         });
     }
